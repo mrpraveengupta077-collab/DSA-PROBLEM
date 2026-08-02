89c2fbf4258c1efd55872e42fa07df2cd2625d59
Praveen Gupta <mrpraveengupta077@gmail.com>
2026-08-02 16:14:46 +0530

    Time: 4 ms (67.56%), Space: 10.1 MB (87.24%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int getvalue(char ch)
+    {
+        switch (ch)
+        {
+            case 'I':
+             return 1;
+            case 'V':
+             return 5;
+            case 'X': 
+            return 10;
+            case 'L':
+             return 50;
+            case 'C':
+             return 100;
+            case 'D':
+             return 500;
+            case 'M':
+             return 1000;
+            default:
+             return 0;
+        }
+    }
+    int romanToInt(string s)
+    {
+        int ans = 0;
+        for (int i = 0; i < s.length() - 1; i++)
+        {
+            if (getvalue(s[i]) < getvalue(s[i + 1]))
+                ans -= getvalue(s[i]);
+            else
+                ans += getvalue(s[i]);
+        }
+
+        ans += getvalue(s[s.length() - 1]);
+        return ans;
+    }
+};
