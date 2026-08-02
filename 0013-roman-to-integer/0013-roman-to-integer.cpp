e6e210dfc9992f0808b6b1af2b69dc3b1e36ec62
Praveen Gupta <mrpraveengupta077@gmail.com>
2026-08-02 16:20:22 +0530

    Time: 0 ms (100%), Space: 9.9 MB (99.43%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -4,22 +4,15 @@ public:
     {
         switch (ch)
         {
-            case 'I':
-             return 1;
-            case 'V':
-             return 5;
-            case 'X': 
-            return 10;
-            case 'L':
-             return 50;
-            case 'C':
-             return 100;
-            case 'D':
-             return 500;
-            case 'M':
-             return 1000;
+            case 'I':return 1;
+            case 'V':return 5;
+            case 'X':return 10;
+            case 'L':return 50;
+            case 'C':return 100;
+            case 'D':return 500;
+            case 'M':return 1000;
             default:
-             return 0;
+            return 0;
         }
     }
     int romanToInt(string s)
