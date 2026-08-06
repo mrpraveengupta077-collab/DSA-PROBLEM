9ed840f700b6f7de730fb12d7d175f2e9a0c671c
Praveen Gupta <mrpraveengupta077@gmail.com>
2026-08-06 21:05:45 +0530

    Time: 0 ms (100%), Space: 7.9 MB (88.11%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -5,19 +5,16 @@ public:
         if (s.length() != goal.length())
             return false;
 
-        vector<string> rotations;
         int n = s.length();
 
         for (int i = 0; i < n; i++) {
-            rotations.push_back(s);
+
+            if (s == goal)
+                return true;
 
             char first = s[0];
             s.erase(0, 1);
             s.push_back(first);
-        }
-        for (int i = 0; i <n; i++) {
-            if (rotations[i] == goal)
-                return true;
         }
 
         return false;
