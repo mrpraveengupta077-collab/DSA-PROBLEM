5e6b0a5cd2680454cf2fe1284b791a75471f2929
Praveen Gupta <mrpraveengupta077@gmail.com>
2026-08-07 22:11:10 +0530

    Time: 0 ms (100%), Space: 7.7 MB (58.89%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        if(n < 2) return false;
+
+        int count = 2;   
+
+        for(int i = 2; i < n; i++) {
+            if(n % i == 0) {
+                count++;
+            }
+        }
+
+        return count == 3;
+    }
+};
