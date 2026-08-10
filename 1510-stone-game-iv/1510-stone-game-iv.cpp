81f98677012ea6ab6da93a3646c551b7a67a2231
Praveen Gupta <mrpraveengupta077@gmail.com>
2026-08-10 21:02:06 +0530

    Time: 43 ms (57.44%), Space: 16.6 MB (45.73%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> dp;
+
+    bool solve(int n) {
+        if (n == 0) {
+            return false;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        for (int i = 1; i * i <= n; i++) {
+            int square = i * i;
+
+            if (solve(n - square) == false) {
+                return dp[n] = true;
+            }
+        }
+
+        return dp[n] = false;
+    }
+
+    bool winnerSquareGame(int n) {
+        dp.resize(n + 1, -1);
+        return solve(n);
+    }
+};
